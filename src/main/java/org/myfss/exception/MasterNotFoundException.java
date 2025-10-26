3315165e07635c2489c300b005bd67be68d5a765
Sasikumar Sahkana <shakana2604@gmail.com>
2025-10-26 19:50:05 +0100

    Ajout d'un apprenti avec son entreprise et maitre d'apprentissage

--- src/main/java/org/myfss/exception/MasterNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.myfss.exception;
-
-public class MasterNotFoundException extends RuntimeException {
-
-    public MasterNotFoundException(Long id) {
-        super("Le master numéro " + id + " est introuvable");
-    }
-}
